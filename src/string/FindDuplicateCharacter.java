50d1d3d8a6821fe99934ae3c4aef6d687a2025fd
parmendra kumar <parmendrakumar718@gmail.com>
2023-05-25 16:27:57 +0530

    Find Duplicate Character

---
+++ src/string/FindDuplicateCharacter.java
@@ -0,0 +1,29 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class FindDuplicateCharacter {
+
+	public static void main(String[] args) {
+		String str="welcome java";
+		
+		char[] charr=str.toCharArray();
+		
+		HashMap<Character, Integer>hm=new HashMap<>();
+		
+		for(char ch:charr) {
+			if(hm.containsKey(ch)) hm.put(ch, hm.get(ch)+1);
+			else hm.put(ch, 1);
+		}
+		
+		System.out.println("all occurance are : \n"+hm);
+		
+		System.out.println("Duplicate values are -");
+		Set<Character> charSet= hm.keySet();
+		for(char ch:charSet) {
+			if(hm.get(ch) >1) System.out.println(ch+" :"+hm.get(ch)+" times");
+		}
+	}
+
+}
