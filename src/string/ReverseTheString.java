534fdfef72060d0218b957d628bc43215c7bcf63
parmendra kumar <parmendrakumar718@gmail.com>
2023-08-16 17:33:33 +0530

    code refactor

--- src/string/ReverseTheString.java
+++ src/string/ReverseTheString.java
@@ -2,22 +2,26 @@ package string;
 
 public class ReverseTheString {
 	public static void main(String[] args) {
-		
-		String str="welcome";
-		
+
+		String str = "welcome";
+
 		// First way with StringBuffer Or StringBuilder
-		StringBuffer buffer=new StringBuffer(str);
+		StringBuffer buffer = new StringBuffer(str);
 		System.out.println(buffer.reverse());
-		
-		// 2nd Way 
-		char[] charr=str.toCharArray();
-		for(int i=charr.length-1; i>=0; i--)
+
+		// 2nd Way
+		char[] charr = str.toCharArray();
+		for (int i = charr.length - 1; i >= 0; i--)
 			System.out.print(charr[i]);
-		
+
 		System.out.println("");
 		// 3rd Way
-		for(int i=str.length()-1; i>=0; i--)
-			System.out.print(str.charAt(i));
+		String output = "";
+		for (int i = str.length() - 1; i >= 0; i--)
+			// System.out.print(str.charAt(i));
+			output += str.charAt(i);
+
+		System.out.println(output);
 	}
 
 }
